848f595653de0de8f9cdbccbeb827d2f57da29c1
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-03-25 08:33:40 +0000

    Deploy to TestEnvironment (Version 0.9.3)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,7 +48,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "icons/Icon-maskable-192.png": "da3a393e1c3c8ace9202e53822af578d",
 "manifest.json": "16796f2cec421f5e50f48fb01663bad3",
 "favicon.png": "7f75a2244b29632fd384b215579c878a",
-"flutter_bootstrap.js": "40e37ea903fa56d190d1f6d1ed8b42ee"};
+"flutter_bootstrap.js": "bffb6358157493aa4da769aeb4656ec0"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
