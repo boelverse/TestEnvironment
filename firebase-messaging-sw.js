e0bcb26797325f29b16cff12997c341ccd252db4
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-03-28 14:44:55 +0000

    Deploy to TestEnvironment (Version 1.0.13)

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -22,16 +22,27 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // Optional:
+let isNotificationDisplayed = false;  // A flag to track if the notification is already displayed
+
 messaging.onBackgroundMessage((payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  // Customize notification
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+
+  // Check if the notification has already been displayed
+  if (!isNotificationDisplayed) {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.icon,
+    };
+
+    // Show notification
+    self.registration.showNotification(notificationTitle, notificationOptions);
+
+    // Set the flag to true after showing the notification
+    isNotificationDisplayed = true;
+  }
 });
+
