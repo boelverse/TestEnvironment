73e95d5d13559a620764f203260e712736fdd99d
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-03-29 19:33:06 +0000

    Deploy to TestEnvironment (Version 1.0.14)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
 
 const messaging = firebase.messaging();
 
@@ -13,6 +13,17 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
+// Listen for background messages from Firebase
+messaging.onBackgroundMessage((payload) => {
+  console.log("[service-worker.js] Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
 // Listen for messages from the app (if needed)
 self.addEventListener("message", (event) => {
