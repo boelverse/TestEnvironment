8e12c1baa8eeffe6e364e2c1e778f29b1f5b5084
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-04-03 11:48:53 +0000

    Deploy to TestEnvironment (Version 1.1.4)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "assets/AssetManifest.json": "e058ab28e3d70ff5f4339b6f13891957",
 "version.json": "2e0cf3a93fabd35d580631aff9d971f9",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "decd1429ecc1506452561b4d01a49658",
+"main.dart.js": "d5dd90b0dde2a8c0b813ddf1e792bfb2",
 "firebase-messaging-sw.js": "c1b0892542b8102f9258345bbfffce6a",
 "icons/Icon-96.png": "7b678632e098a409e3ef73cc0fb2b04e",
 "icons/Icon-maskable-512.png": "72f0ddf85c0feb650daf4a58422fd399",
@@ -49,7 +49,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "icons/Icon-maskable-192.png": "da3a393e1c3c8ace9202e53822af578d",
 "manifest.json": "16796f2cec421f5e50f48fb01663bad3",
 "favicon.png": "7f75a2244b29632fd384b215579c878a",
-"flutter_bootstrap.js": "130785b9a553c66291b605c3a349480b"};
+"flutter_bootstrap.js": "a983185b533fac9ffe9e78f3c33121ee"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
