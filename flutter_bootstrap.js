70e0293ab725475a5fc4f6e8c1c593c24ebebe3b
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-04-24 23:53:00 +0000

    Deploy to TestEnvironment (Version 1.1.11)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "602077660"
+    serviceWorkerVersion: "3432008183"
   }
 });
