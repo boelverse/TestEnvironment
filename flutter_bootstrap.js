a4a3326792800cef1592a099cc547d6800d154ac
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-05-04 12:59:39 +0000

    Deploy to TestEnvironment (Version 1.1.14)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1290571341"
+    serviceWorkerVersion: "3837531726"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"firebase-messaging-sw.js": "ccd54d7c103493747f266f47f6eed9ed
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "index.html": "4066279985502d729b34001401b8fe19",
 "/": "4066279985502d729b34001401b8fe19",
-"flutter_bootstrap.js": "8c5fa56d4044d82a01140cb31f2b8c1c",
+"flutter_bootstrap.js": "888c8ab88255d4e5e8871ef2f03f5cbf",
 "icons/Icon-72.png": "b01f313c0225573dc40498e4ec262d8b",
 "icons/Icon-192.png": "da3a393e1c3c8ace9202e53822af578d",
 "icons/Icon-144.png": "77650e54f9fbdde69dde7fe7ffb934e9",
@@ -49,7 +49,7 @@ const RESOURCES = {"firebase-messaging-sw.js": "ccd54d7c103493747f266f47f6eed9ed
 "icons/Icon-512.png": "72f0ddf85c0feb650daf4a58422fd399",
 "favicon.png": "7f75a2244b29632fd384b215579c878a",
 "service-worker.js": "7c2434e70de19d591241e61528951351",
-"main.dart.js": "8f5439a0344204c0a815aabf09c4e34f"};
+"main.dart.js": "bbecc51230ca7d687a49540afeb935f7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
